a15fe04b5e0ac74a056b40a3843991f9ab6083d4
zhangyunkai <yxlc785@163.com>
2020-04-25 10:54:17 +0800

    bannmer ok

---
+++ pages/home/home.js
@@ -0,0 +1,16 @@
+
+//index.js
+//获取应用实例
+
+Page({
+  data: {
+	homeBannerList:[
+	
+		{"img":"http://a0.att.hudong.com/64/76/20300001349415131407760417677.jpg","name":"zhangsan"},
+		{"img":"https://ss3.bdstatic.com/70cFv8Sh_Q1YnxGkpoWK1HF6hhy/it/u=3125526008,3010441145&fm=26&gp=0.jpg","name":"lisi"},
+		]
+  }
+  
+ 
+ 
+})
