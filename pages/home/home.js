c2816819201eb39c5d7eabee7c02e33d97b78347
zhangyunkai <yxlc785@163.com>
2020-04-25 21:04:05 +0800

    home

--- pages/home/home.js
+++ pages/home/home.js
@@ -5,10 +5,13 @@
 Page({
   data: {
 	homeBannerList:[
-	
-		{"img":"http://a0.att.hudong.com/64/76/20300001349415131407760417677.jpg","name":"zhangsan"},
-		{"img":"https://ss3.bdstatic.com/70cFv8Sh_Q1YnxGkpoWK1HF6hhy/it/u=3125526008,3010441145&fm=26&gp=0.jpg","name":"lisi"},
-		]
+			{"img":"http://a0.att.hudong.com/64/76/20300001349415131407760417677.jpg","name":"zhangsan"},
+			{"img":"https://ss3.bdstatic.com/70cFv8Sh_Q1YnxGkpoWK1HF6hhy/it/u=3125526008,3010441145&fm=26&gp=0.jpg","name":"lisi"},
+		],
+	tabList:[
+		{"img":"../../image/navi-tab1.png","content":"意见反馈"},
+		{"img":"../../image/navi-tab2.png","content":"使用指南"}
+	]	
   }
   
  
